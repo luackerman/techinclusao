2c17d26fd0ba8be81457e639d34d4ef2f6c2d6a0
luackerman <hyugaackermanvitorialuiza@gmail.com>
2024-11-13 22:07:02 -0300

    inicio2

--- src/app/bot/actionProvider.jsx
+++ src/app/bot/actionProvider.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 
 const ActionProvider = ({ createChatBotMessage, setState, children }) => {
+  const initialAction =() => {
+    const message = createChatBotMessage('Insira o seu nome para começar!')
+    updateState(message)
+  }  
+
+  const updateState = (message) => {
+    setState((prev) => ({
+      ...prev,
+      messages: [...prev.messages, message],
+    }));
+}
+
   return (
     <div>
       {React.Children.map(children, (child) => {
         return React.cloneElement(child, {
-          actions: {},
+          actions: {
+            initialAction,
+          },
         });
       })}
     </div>
   );
 };
-
-export default ActionProvider;
